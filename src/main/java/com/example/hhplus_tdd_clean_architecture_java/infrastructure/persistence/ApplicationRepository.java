fece728c1121f043ed9fddf1dd4b1c92305e9904
caoyu <choyumin01@gmail.com>
2024-10-03 23:00:50 +0900

    fix. repository 수정
    - 비관적 락 적용
    - 위치 변경

--- src/main/java/com/example/hhplus_tdd_clean_architecture_java/infrastructure/persistence/ApplicationRepository.java
+++ src/main/java/com/example/hhplus_tdd_clean_architecture_java/infrastructure/persistence/ApplicationRepository.java
@@ -1,9 +1,15 @@
 package com.example.hhplus_tdd_clean_architecture_java.infrastructure.persistence;
 
+import com.example.hhplus_tdd_clean_architecture_java.domain.lecture.Application;
+import com.example.hhplus_tdd_clean_architecture_java.domain.lecture.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ApplicationRepository extends JpaRepository<ApplicationEntity, Long> {
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
     boolean existsByUserIdAndLectureId(Long userId, Long lectureId);
+    long countByLecture(Lecture lecture);
+    List<Application> findByUserId(Long userId);
 }
